9681a48dba8f27a4984899b6cbb29c5f01a388bb
HOMETAKESHI <rsa59018@nifty.com>
2023-03-09 22:13:02 +0900

    最初のコミット

---
+++ main.go
@@ -0,0 +1,51 @@
+// STEP11: 集計ページの作成
+
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+
+	_ "github.com/tenntenn/sqlite"
+
+	_ "github.com/bmizerany/pat"
+)
+
+func main() {
+
+	// データベースへ接続
+	// ドライバにはSQLiteを使って、
+	// accountbook.dbというファイルでデータベース接続を行う
+	db, err := sql.Open("sqlite", "accountbook.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// AccountBookをNewAccountBookを使って作成
+	ab := NewAccountBook(db)
+
+	// テーブルを作成
+	if err := ab.CreateTable(); err != nil {
+		log.Fatal(err)
+	}
+
+	// HandlersをNewHandlersを使って作成
+	hs := NewHandlers(ab)
+
+	// ハンドラの登録
+	http.HandleFunc("/", hs.ListHandler)
+	http.HandleFunc("/ws", hs.WsEndpoint)
+	http.HandleFunc("/save", hs.SaveHandler)
+	http.HandleFunc("/summary", hs.SummaryHandler)
+
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	go ListenToWsChannel(hs)
+
+	fmt.Println("http://localhost:8080 で起動中...")
+	// HTTPサーバを起動する
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
