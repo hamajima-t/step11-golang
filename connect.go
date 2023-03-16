a51c1f6631b9485827c7b5a4a67dcbf9ed91bf1e
TakeshiHamajima <slink-011@slink-inc.com>
2023-03-16 09:50:25 +0900

    WebSocketsのコネクション情報を格納するための構造体を追加

--- connect.go
+++ connect.go
@@ -1,5 +1,12 @@
 package main
 
+import "github.com/gorilla/websocket"
+
+// WebSocketsコネクション情報を格納
+type WebSocketConnection struct {
+	*websocket.Conn
+}
+
 // ブラウザから送信されたデータを格納
 type WsPayload struct {
 	Action string
