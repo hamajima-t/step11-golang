68a53f107ec1093f6693b014bee2e68114c00be4
TakeshiHamajima <slink-011@slink-inc.com>
2023-04-05 11:31:48 +0900

    WebSocketからデータを受け取りDOMを更新する

--- static/main.js
+++ static/main.js
@@ -26,7 +26,7 @@
       const heading = document.getElementById('latest10');
       updateTable(tbodies, data);
       updateHeading(heading,
-        `最新${data.Items.length}件(<a href="/summary/">集計</a>)`
+        `最新${data.Items.length}件(<a href="/summary">集計</a>)`
       );
     });
 
@@ -45,27 +45,30 @@
   }
 
   function updateTable(tbodies, data) {
+    // bodyの内容を全て削除する
     while (tbodies[0].firstChild) {
       tbodies[0].removeChild(tbodies[0].firstChild);
     }
 
+    // DBにデータがあれば、bodyの内容を作成する
     if (data.Items.length > 0) {
-      Object.keys(data.Items).forEach(function(key) {
+      Object.keys(data.Items).forEach(function(index) {
         const tr = document.createElement('tr');
-        tr.dataset.id = data.Items[key].ID;
-        const item = {
-          category: data.Items[key].Category,
-          price: data.Items[key].Price,
-          delete: "x"
-        };
-
-        Object.keys(item).forEach(function(key) {
-          const td = document.createElement('td');
-          const textNode = document.createTextNode(item[key]);
-          if (item[key] === "x") { td.setAttribute("class", "delete_item"); }
-          td.appendChild(textNode);
-          tr.appendChild(td);
+        tr.dataset.id = data.Items[index].ID;
+        Object.keys(data.Items[index]).forEach(function(key) {
+          if (key !== "ID") {
+            const td = document.createElement('td');
+            const textNode = document.createTextNode(data.Items[index][key]);
+            td.appendChild(textNode);
+            tr.appendChild(td);
+          }
         });
+
+        const td = document.createElement('td');
+        const textNode = document.createTextNode("x");
+        td.setAttribute("class", "delete_item");
+        td.appendChild(textNode);
+        tr.appendChild(td);
 
         tbodies[0].appendChild(tr);
       });
